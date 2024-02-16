43711ab619434f4e10f415beb28f6b1c75a70443
Den <90329284+zadumka@users.noreply.github.com>
2024-02-16 18:25:53 +0200

    create new link

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Homepage from "./pages/Homepage";
+import "./index.css";
+import { BrawserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Brawserhhjh>
+      < Homepage/>
+    </Brawserhhjh>
+  </React.StrictMode>
+);
